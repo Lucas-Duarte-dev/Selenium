7bc365bb625d93189cbd03f4954777bb52d6fb24
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-05-06 20:25:00 -0300

    Acessando a UNICSUL com Selinium

--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,14 @@
 import { Builder, By, Key, until } from 'selenium-webdriver';
 
-(async function exemple() {
+(async function accessUniqsul() {
   let driver = await new Builder().forBrowser('chrome').build();
 
   try {
     await driver.get('http://google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-    await driver.get('http://google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('TORANJA', Key.RETURN);
-    await driver.wait(until.titleIs('TORANJA - Google Search'), 1000);
+    await driver.findElement(By.name('q')).sendKeys('Cruzeiro do Sul', Key.RETURN);
+    await driver.wait(until.titleIs('Cruzeiro do Sul - Google Search'), 1000);
+    await driver.findElement(By.xpath("//a[@data-pcu='https://www.cruzeirodosulvirtual.com.br/']")).click();
+
   } finally {
     await driver.quit()
   }
