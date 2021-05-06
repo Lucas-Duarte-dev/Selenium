885720fd3a4907805eac52672f89967bf59a23e5
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-05-05 22:18:26 -0300

    Criando testes no browser

--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,15 @@
 import { Builder, By, Key, until } from 'selenium-webdriver';
-import chrome from 'selenium-webdriver/chrome'
-
-
 
 (async function exemple() {
   let driver = await new Builder().forBrowser('chrome').build();
 
   try {
-    await driver.get('http://google.com/');
+    await driver.get('http://google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.get('http://google.com/ncr');
+    await driver.findElement(By.name('q')).sendKeys('TORANJA', Key.RETURN);
+    await driver.wait(until.titleIs('TORANJA - Google Search'), 1000);
   } finally {
-    await driver.quit()
   }
 })()
