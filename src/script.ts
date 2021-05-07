ddf5553607d305986119e6e17b4c4f9784f6e9fa
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-05-06 21:14:17 -0300

    Acessando os cursos da cruzeiro

--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,23 @@
-import { Builder, By, Key, until } from 'selenium-webdriver';
+import { Builder, By, Key, until, Actions, WebDriver, WebElement } from 'selenium-webdriver';
+import { Driver } from 'selenium-webdriver/chrome';
 
 (async function accessUniqsul() {
   let driver = await new Builder().forBrowser('chrome').build();
+
 
   try {
     await driver.get('http://google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys('Cruzeiro do Sul', Key.RETURN);
     await driver.wait(until.titleIs('Cruzeiro do Sul - Google Search'), 1000);
     await driver.findElement(By.xpath("//a[@data-pcu='https://www.cruzeirodosulvirtual.com.br/']")).click();
+    const classGraduation = await driver.findElement(By.id('cardapio-mosaico0'))
+    const getRoute = await classGraduation.getAttribute('href');
+    await driver.get(getRoute)
+    const getADS = await driver.findElements(By.className('vitrine-graduacao-cards'))
+    console.log(getADS)
+
 
   } finally {
-    await driver.quit()
+
   }
 })()
