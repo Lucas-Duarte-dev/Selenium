dd56274c619551fe7b30afdaeffb8293f9b93a20
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-05-09 14:31:00 -0300

    Get info ADS

--- src/script.ts
+++ src/script.ts
@@ -1,27 +1,31 @@
-import { Builder, By, Key, until, Actions, WebDriver, WebElement } from 'selenium-webdriver';
-import { Driver } from 'selenium-webdriver/chrome';
+import { Builder, By, Key, until } from 'selenium-webdriver';
 
-(async function accessUniqsul() {
-  let driver = await new Builder().forBrowser('chrome').build();
-
+(async function accessUniqsul(): Promise<string> {
+  const driver = await new Builder().forBrowser('chrome').build();
 
   try {
     await driver.get('http://google.com/ncr');
     await driver.findElement(By.name('q')).sendKeys('Cruzeiro do Sul', Key.RETURN);
     await driver.wait(until.titleIs('Cruzeiro do Sul - Google Search'), 1000);
     await driver.findElement(By.xpath("//a[@data-pcu='https://www.cruzeirodosulvirtual.com.br/']")).click();
-    const classGraduation = await driver.findElement(By.id('cardapio-mosaico0'))
-    const getRoute = await classGraduation.getAttribute('href');
-    await driver.get(getRoute)
-    const getADS = await driver.findElements(By.className('curso-card-btn'))
-    const searchCourse =  getADS.map(element => element)
-    const getRouteADS = await searchCourse[6].getAttribute('href');
-    await driver.get(getRouteADS)
 
-    const getText = await driver.findElement(By.className('scconteudo-texto-principal geral-conteudolista short-mobile'))
-    console.log(await getText.getText())
+    const classGraduation = await driver.findElement(By.id('cardapio-mosaico0'));
+    const getRoute = await classGraduation.getAttribute('href');
+
+    await driver.get(getRoute);
+
+    const getADS = await driver.findElements(By.className('curso-card-btn'));
+    const searchCourse = getADS.map(element => element);
+    const getRouteADS = await searchCourse[6].getAttribute('href');
+    await driver.get(getRouteADS);
+
+    const text = await driver.findElement(By.className('scconteudo-texto-principal geral-conteudolista short-mobile')).getText();
+
+    console.log(text)
+
+    return text;
 
   } finally {
-
+    await driver.quit()
   }
 })()
