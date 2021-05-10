54337e89e370c2e186f897a2e883a339b07e8d1c
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2021-05-10 18:00:36 -0300

    refectory

--- src/script.ts
+++ src/script.ts
@@ -1,15 +1,22 @@
 import { Builder, By, Key, until } from 'selenium-webdriver';
 
+import os from 'os'
+
+const driver = new Builder().forBrowser(os.platform() === 'linux' ? 'chrome' : 'firefox').build();
+
+const getUrl = (url: string): Promise<void> => {
+  return driver.get(`${url}`)
+}
+
 (async function accessUniqsul(): Promise<string> {
-  const driver = await new Builder().forBrowser('chrome').build();
 
   try {
-    await driver.get('http://google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('Cruzeiro do Sul', Key.RETURN);
+    await getUrl('http://google.com/ncr');
+    driver.findElement(By.name('q')).sendKeys('Cruzeiro do Sul', Key.RETURN);
     await driver.wait(until.titleIs('Cruzeiro do Sul - Google Search'), 1000);
     await driver.findElement(By.xpath("//a[@data-pcu='https://www.cruzeirodosulvirtual.com.br/']")).click();
 
-    const classGraduation = await driver.findElement(By.id('cardapio-mosaico0'));
+    const classGraduation = driver.findElement(By.id('cardapio-mosaico0'));
     const getRoute = await classGraduation.getAttribute('href');
 
     await driver.get(getRoute);
@@ -17,11 +24,9 @@ import { Builder, By, Key, until } from 'selenium-webdriver';
     const getADS = await driver.findElements(By.className('curso-card-btn'));
     const searchCourse = getADS.map(element => element);
     const getRouteADS = await searchCourse[6].getAttribute('href');
-    await driver.get(getRouteADS);
+    await getUrl(getRouteADS);
 
     const text = await driver.findElement(By.className('scconteudo-texto-principal geral-conteudolista short-mobile')).getText();
-
-    console.log(text)
 
     return text;
 
